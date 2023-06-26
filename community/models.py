dae40d1225b323e141fc667e7558e64e3d89e5e5
Eduardo-Antoniassi-Lobato <>
2023-06-26 20:01:08 +0000

    add tag class to models

--- community/models.py
+++ community/models.py
@@ -14,6 +14,10 @@ class Author(models.Model):
     email = models.EmailField()
 
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150, unique=True)
     excerpt = models.CharField(max_length=180)
