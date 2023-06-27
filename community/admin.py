f923e6dafa04c24da0ec35d3c8a11aa3f62c6ec2
Eduardo-Antoniassi-Lobato <>
2023-06-27 15:40:31 +0000

    add features to admin panel

--- community/admin.py
+++ community/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Author, Tag
+from .models import Post, Author, Tag, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -7,9 +7,21 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('title', 'slug', 'status', 'date')
+    list_filter = ('title', 'status', 'date', 'author', 'tags')
+    list_display = ('author', 'date', 'title', 'status')
     summernote_fields = ('content')
-    search_fields = ['title', 'content']
+    search_fields = ['title', 'content', 'author', 'date']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_filter = ('name', 'post', 'date', 'approved')
+    search_fields = ('name', 'post', 'date')
+    actions = ['approved_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
 
 
 admin.site.register(Author)
--- community/views.py
+++ community/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import date
 # Create your views here.
 
 
