93fb901d77ef4af06758dffd97377a5534957e84
Eduardo-Antoniassi-Lobato <>
2023-07-01 16:36:04 +0000

    Add starting templates

--- community/views.py
+++ community/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from datetime import date
+from django.views import generic
+from .models import Post, Comment, Tag, Likes
+
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-date')
+    template_name = 'index.html'
+    paginate_by = 7
+    
 
 
 def index_page(request):
