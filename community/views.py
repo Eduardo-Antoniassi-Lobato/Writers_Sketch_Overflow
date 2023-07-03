fa377e75d1882b2c5adc7c20c4c7cad96350541b
Eduardo-Antoniassi-Lobato <>
2023-07-03 15:05:11 +0000

    finish func. get to PostItem class

--- community/views.py
+++ community/views.py
@@ -22,4 +22,13 @@ class PostItem(View):
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
- 
+
+        return render(
+            request,
+            "post_item.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
