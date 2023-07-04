d60672cc36d80395e523fda7fc42526185fcfc5b
Eduardo-Antoniassi-Lobato <>
2023-07-04 20:01:09 +0000

    Correct indentation in PostLikes

--- community/views.py
+++ community/views.py
@@ -74,9 +74,9 @@ class PostLike(View):
     def post(self, request, slug):
         post = get_object_or_404(Post, slug=slug)
 
-    if post.likes.filter(id=request.user.id).exists():
-        post.likes.remove(request.user)
-    else:
-        post.likes.add(request.user)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
 
-    return HttpResponseRedirect(reverse('post_item', args=[slug]))
+        return HttpResponseRedirect(reverse('post_item', args=[slug]))
