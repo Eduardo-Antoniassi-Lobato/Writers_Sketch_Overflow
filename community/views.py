5007b8a068f9d057312e28662a0d41281d1b34fa
Eduardo-Antoniassi-Lobato <>
2023-07-04 13:56:15 +0000

    Correction typo PostItem class

--- community/views.py
+++ community/views.py
@@ -16,7 +16,7 @@ class PostList(generic.ListView):
 
 class PostItem(View):
 
-    def get(self, request, slug, *args, **kargs):
+    def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-date")
