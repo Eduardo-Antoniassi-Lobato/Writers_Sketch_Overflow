cf8fd4f41cefdb9ef0428641c095648af429fb6f
Eduardo-Antoniassi-Lobato <>
2023-07-22 14:10:00 +0000

    Add writersusers app

--- community/urls.py
+++ community/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
-    path("<slug:slug>/", views.PostItem.as_view(), name="post_item"),
-    path("like/<slug:slug>", views.PostLike.as_view(), name="post_like")
 ]
--- writers_sketch_overflow/settings.py
+++ writers_sketch_overflow/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'crispy_forms',
     'community',
+    'writersusers',
 ]
 
 SITE_ID = 1
