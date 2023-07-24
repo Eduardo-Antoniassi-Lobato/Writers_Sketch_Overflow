c1a4e5e9007eae40ee3a5f034330f14ebe5466c9
Eduardo-Antoniassi-Lobato <>
2023-07-24 08:40:34 +0000

    Add ready func to apps.py

--- writersusers/apps.py
+++ writersusers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class WritersusersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'writersusers'
+
+    def ready(self):
+        import writersusers.signals
