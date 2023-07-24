75368ac6a16042047b1a9d4750807f41a3ec8c5a
Eduardo-Antoniassi-Lobato <>
2023-07-24 13:19:56 +0000

    Add ckeditor to models.py

--- community/models.py
+++ community/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 from cloudinary.models import CloudinaryField
-
-STATUS = ((0, "Draft"), (1, "published"))
+from ckeditor.fields import RichTextField
 
 
 class Author(models.Model):
